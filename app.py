59a0dedc8406301fe71420a0e5dbbb01a7fafed6
JuanStucker <juan.stucker1@gmail.com>
2022-02-19 15:48:49 +0100

    Changed structure of repository, data will need to be downloaded a-posteriori, included .dockerignore file for building image without the training data

--- app.py
+++ app.py
@@ -14,97 +14,18 @@ import speech_recognition as sr
 from flask_swagger_ui import get_swaggerui_blueprint
 from pydub import AudioSegment
 import logging
+# global variables referenced in the functions
+# filename_wav = ""
+# filename_mp3 = ""
+# emotion_array = ""
+# text_result = ""
 
-
-def handle_audio(json_data): 
-# this function should detect the type of audio file the request sent, and decode it correctly from base64 into a PL wav format.
-    name = json_data["fileName"]
-    audio_64 = json_data["fileBody"]
-    fileType = name[-3:]
-    if  fileType == "mp3":
-        try: 
-            mp3_file_temp = open("temp.mp3", "wb")
-            audio_decoded = base64.b64decode(audio_64)
-            mp3_file_temp.write(audio_decoded)
-            # APP.logger.info("The final object type is: " + str(type(mp3_file_temp)))
-            # APP.logger.info("Until here everything should be good!! and the base 64 should be decoded into something")
-            filename_mp3 = "temp.mp3"
-            filename_wav = "newtemp.wav"
-
-            # converting mp3 to wav
-            src = filename_mp3
-            dst = filename_wav
-            sound = AudioSegment.from_mp3(src)
-            sound.export(dst, format = "wav")
-            # APP.logger.info("Correctly changed format from MP3 to WAV!, approaching next step")
-            # APP.logger.info(filename_wav)
-
-        except Exception as ex: 
-            pass
-            #APP.logger.info("Could not convert filetype " + file_type+ " to .wav")
-
-    if fileType == "m4a": 
-        try: 
-            m4a_file_temp = open("temp.m4a", "wb")
-            audio_decoded = base64.b64decode(audio_64)
-            m4a_file_temp.write(audio_decoded)
-            # APP.logger.info("The final object type is: " + str(type(mp3_file_temp)))
-            # APP.logger.info("Until here everything should be good!! and the base 64 should be decoded into something")
-            filename_m4a = "temp.m4a"
-            filename_wav = "newtemp.wav"
-
-            # converting mp3 to wav
-            src = filename_m4a
-            dst = filename_wav
-            sound = AudioSegment.from_file(filename_m4a, format = "m4a")
-            sound.export(dst, format = "wav")
-            # APP.logger.info("Correctly changed format from MP3 to WAV!, approaching next step")
-            # APP.logger.info(filename_wav)
-
-            
-
-        except Exception as ex:
-            print(ex) 
-
-    return filename_wav
-
-# logger_emo = logging.getLogger("deep_emotion_recognition")
-# logger_emo.setLevel(logging.CRITICAL)
-# logger_emo.propagate = False
-
-#logging.getLogger("APP").setLevel(logging.INFO)
-
-
-
-
-
-
-LANGUAGE = "en"
+LANGUAGE = "en" #for the speech to text engine
 MONGO_HOST = "137.226.232.75"
 MONGO_PORT = 32112
 
+
 APP = Flask(__name__)
-recognizer = sr.Recognizer()
-
-
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-handler = logging.StreamHandler(sys.stdout)
-logger.addHandler(handler)
-
-
-try: 
-    APP.logger.info("Training the model")
-    deepemo = DeepEmotionRecognizer(emotions=['angry', 'sad', 'neutral', 'ps', 'happy'], n_rnn_layers=2, n_dense_layers=2, rnn_units=128, dense_units=128, verbose = False)
-    deepemo.train()
-    print("Test accuracy score: {:.3f}%".format(deepemo.test_score()*100))
-except Exception as ex: 
-    APP.logger.info(ex)
-# detector = EmotionRecognizer(emotions=["sad", "neutral","happy", "angry", "bored"], verbose=0)
-# detector.train()
-# print("Test accuracy score: {:.3f}%".format(detector.test_score()*100))
-
-
 
 #SWAGGER initialization
 SWAGGER_URL = "/swagger"
@@ -118,25 +39,130 @@ SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
 )
 APP.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
 
-#speech emotion recognition training
+def handle_audio(json_data): 
+# this function should detect the type of audio file the request sent, and decode it correctly from base64 into a PL wav format.
+    name = json_data["fileName"]
+    audio_64 = json_data["fileBody"]
+    fileType = name[-3:]
+    if  fileType == "mp3":
+        try: 
+
+            audio_decoded = base64.b64decode(audio_64)
+
+            filename = "temp.mp3"
+            filename_export = "wavtemp.wav"
+
+            file_temp = open(filename_export, "wb")
+            file_temp.write(audio_decoded)
+
+            # converting mp3 to wav
+            src = filename
+            dst = filename_export
+            sound = AudioSegment.from_mp3(src)
+            sound = sound.split_to_mono()[0]
+            sound.export(dst, format =  "wav")
+
+        except Exception as ex: 
+            APP.logger.info(ex)
+            APP.logger.info("Error with filetype: mp3")
+
+    if (fileType == "m4a"): 
+        try: 
+
+            audio_decoded = base64.b64decode(audio_64)
+
+            filename = "temp.m4a"
+            filename_export = "wavtemp.wav"
+
+            file_temp = open(filename, "wb")
+            file_temp.write(audio_decoded)
+            APP.logger.info("Until now no problem")
+
+            # converting m4a to wav
+            src = filename
+            dst = filename_export
+            try: 
+                sound = AudioSegment.from_file(src, format ="m4a")
+                sound = sound.split_to_mono()[0]
+                APP.logger.info("SUCESS: audio decoding")
+            except: 
+                sound = AudioSegment.from_file(src, format = "mp4")
+                APP.logger.info("Changed to mp4 format")    
+            sound.export(dst, format = "wav")
+            
 
 
-# print(os.getcwd())
-# def get_estimators_name(estimators):
-#     result = [ '"{}"'.format(estimator.__class__.__name__) for estimator, _, _ in estimators ]
-#     return ','.join(result), {estimator_name.strip('"'): estimator for estimator_name, (estimator, _, _) in zip(result, estimators)}
+        except Exception as ex:
+            APP.logger.info(ex)
+            APP.logger.info("m4a")
 
-# estimators = get_best_estimators(True)
-# estimators_str, estimator_dict = get_estimators_name(estimators)
-# features = ["mfcc", "chroma", "mel"]
-# detector = EmotionRecognizer(estimator_dict["BaggingClassifier"], emotions=["sad", "neutral","happy", "ps", "angry"], features=features, verbose=0)
+    if (fileType == "aac"): 
+        try: 
+
+            audio_decoded = base64.b64decode(audio_64)
+
+            filename = "temp.aac"
+            filename_export = "wavtemp.wav"
+
+            file_temp = open(filename, "wb")
+            file_temp.write(audio_decoded)
+            APP.logger.info("Until now no problem")
+
+            # converting aac to wav
+            src = filename
+            dst = filename_export
+            try: 
+                sound = AudioSegment.from_file(src, format ="aac")
+                sound = sound.split_to_mono()[0]
+                APP.logger.info("SUCESS: audio decoding")
+            except: 
+                sound = AudioSegment.from_file(src, format = "aac")
+                APP.logger.info("Changed to mp4 format")    
+            sound.export(dst, format = "wav")
+            
+
+
+        except Exception as ex:
+            APP.logger.info(ex)
+            APP.logger.info("aac")            
+ 
+
+    return filename_export
+
+#Changing log level for the emotion recognition module
+# logger_emo = logging.getLogger("deep_emotion_recognition")
+# logger_emo.setLevel(logging.CRITICAL)
+# logger_emo.propagate = False
+
+#Change the log level of this app
+#logging.getLogger("APP").setLevel(logging.INFO)
+
+
+
+#Speech to text recognizer
+recognizer = sr.Recognizer()
+
+#Changin log configuration to show logs from flask
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+handler = logging.StreamHandler(sys.stdout)
+logger.addHandler(handler)
+
+
+#try: 
+APP.logger.info("Training the model")
+deepemo = DeepEmotionRecognizer(emotions=['angry', 'sad', 'neutral', 'ps', 'happy'], n_rnn_layers=2, n_dense_layers=2, rnn_units=128, dense_units=128, verbose = False)
+deepemo.train()
+print("Test accuracy score: {:.3f}%".format(deepemo.test_score()*100))
+#except Exception as ex: 
+#APP.logger.info(ex)
+# detector = EmotionRecognizer(emotions=["sad", "neutral","happy", "angry", "bored"], verbose=0)
 # detector.train()
 # print("Test accuracy score: {:.3f}%".format(detector.test_score()*100))
 
 
 
-
-#Connecting with the database# WIll have to comment that in once it works with kube
+#Connection to the Databse
 try: 
     APP.logger.info("Trying to connect with the database...")
     mongo = pymongo.MongoClient(
@@ -146,10 +172,9 @@ try:
          )
     APP.logger.info("Checking connection with the server")
     mongo.server_info()  #triggers the exception if no connection
-    print("Until now, everything is good")
     db = mongo.course
     db_emotion = mongo.emotion
-    print("SUCCESS: connecting with the DB!")
+    APP.logger.info("SUCCESS: connecting with the DB!")
 
 except Exception as ex:    
     APP.logger.info("ERROR: Cannot connect to the databse") 
@@ -168,11 +193,11 @@ def speech_emotion_recognition():
         try:
             json_data = request.get_json(force = True) #output is of type DICT
         #     data_type = json_data["fileName"]
-        #     audio = json_data["fileBody"]
         #     #course_id = json_data["course_id"]
         #     #item_id = json_data["item_id"] #optional parameter 
-            user_id = json_data["user"]
+            user_mail = json_data["email"]
             time = datetime.datetime.now()
+      
         except Exception as ex: 
         #     APP.logger.info("Something went wrong extracting JSON data")
             APP.logger.info(ex)    
@@ -202,23 +227,22 @@ def speech_emotion_recognition():
         # APP.logger.info(filename_wav)
 
         filename_wav = handle_audio(json_data)
-        text_result = ""
         # speech to text
+        text_result = "Not found"
         try: 
             with sr.AudioFile(filename_wav) as source: 
                 audio_data = recognizer.record(source)
                 text_result = recognizer.recognize_google(audio_data, language = LANGUAGE)
-                APP.logger.info("****************The SPEECH TO TEXT result is ************")
+                APP.logger.info("****************The SPEECH TO TEXT result is *******************")
                 APP.logger.info(text_result)
         except Exception as ex:
             APP.logger.info(ex)
             APP.logger.info("Something went wrong trying to perform speech to text")
-            APP.logger.info(ex) 
 
 
 
         
-
+        emotion_array = ""
         # recognizing emotion
         try: 
             emotion_array = deepemo.predict_proba(filename_wav)
@@ -238,9 +262,9 @@ def speech_emotion_recognition():
         #      "coeffs"],
         #       "valence": 1,
         #        "time": "today",
-                "text": (text_result + "| predicted_emotion:" + str(emotion_array))
+                "text": ("predicted text:" + text_result + " | predicted_emotion:" + str(emotion_array))
                 }
-        update_db = {"user_id": user_id, 
+        update_db = {"user_id": user_mail, 
         # "course_id":course_id,
         #  "item_id": item_id,
         #   "audio_type": data_type,
@@ -275,7 +299,7 @@ def speech_emotion_recognition():
         #  )
 
     except Exception as ex:
-        print(ex) 
+        APP.logger.info(ex) 
 
 
 @APP.route("/static/emotion/speech/<fileName>/", methods = ["DELETE"])
@@ -301,27 +325,27 @@ def text_emotion_recognition():
 
 
 #MONGODB query handler for the emotion from a user for a specif item
-# @APP.route("static/emotion/<user>/<item>/", methods = ["GET"])
+@APP.route("/static/emotion/<user>/<item>/", methods = ["GET"])
 
-# def get_emotion_data(user, item): 
-#     try: 
+def get_emotion_data(user, item): 
+    try: 
 
-#         query = db_emotion.users.find( {"user_id": user, "item_id":item} )
-#         print("Before being converted the query was of type: "+str(type(query)))
-#         query_list = list(query)[0] # this is of type DICT, so searching should be easy
-#         query_json = {"Emotion": query_list["emotion"], "Valence": query_list["valence"]
-#         }
+        query = db_emotion.users.find( {"user_id": user, "item_id":item} )
+        APP.logger.info("Before being converted the query was of type: "+str(type(query)))
+        query_list = list(query)[0] # this is of type DICT, so searching should be easy
+        query_json = {"Emotion": query_list["emotion"], "Valence": query_list["valence"]
+        }
 
-#         #return json.dumps(query_json)
+        #return json.dumps(query_json)
 
-#         return Response(
-#         response = json.dumps(query_json), 
-#         status = 200, 
-#         mimetype = "application/json"
-#         )
-#     except Exception as ex: 
-#         print("Coould not query data: ")
-#         print(ex)    
+        return Response(
+        response = json.dumps(query_json), 
+        status = 200, 
+        mimetype = "application/json"
+        )
+    except Exception as ex: 
+        APP.logger.info("Coould not query data: ")
+        APP.logger.info(ex)    
 
 @APP.route("/static/test/", methods = ["POST"])
 def test_json(): 
--- deep_emotion_recognition.py
+++ deep_emotion_recognition.py
@@ -3,7 +3,7 @@ import os
 import sys
 stderr = sys.stderr
 sys.stderr = open(os.devnull, 'w')
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+#os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import tensorflow as tf
 
 from tensorflow.keras.layers import LSTM, GRU, Dense, Activation, LeakyReLU, Dropout
